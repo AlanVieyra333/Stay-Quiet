c05a4be052940bb3ec65fe86a943de5697148229
Fernando Vieyra <alan.vieyra376@gmail.com>
2017-11-07 12:51:28 -0600

    CU3: En desarrollo.

--- app/src/main/java/androides/stayquiet/HomeActivity.java
+++ app/src/main/java/androides/stayquiet/HomeActivity.java
@@ -1,8 +1,12 @@
 package androides.stayquiet;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +21,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         final Intent intentMaps = new Intent(HomeActivity.this, MapsActivity.class);
         tvNameMine = (TextView) findViewById(R.id.tvNameMine);
@@ -34,6 +47,31 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intentMaps);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_profile:
+                Intent profile = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(profile);
+                return true;
+            case R.id.menu_settings:
+                // Change it.
+                return true;
+            case R.id.menu_close_session:
+                // Change it.
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getParams(){
